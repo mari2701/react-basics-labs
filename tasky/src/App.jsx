94864811722f835a4bb11bb51eb31be8e8e17b7f
Maria <20106500@mail.wit.ie>
2026-01-28 14:26:04 +0000

    Mark task as done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -6,9 +6,9 @@ function App() {
 
    const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { id: 3, title: "Tidy up", deadline: "Today" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority: "Medium" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Low" },
+      { id: 3, title: "Tidy up", deadline: "Today", priority: "High" }
     ]
   });
  
@@ -20,6 +20,7 @@ function App() {
       title={task.title}
       description={task.description}
       deadline={task.deadline}
+      priority={task.priority}
       key={task.id}
     />
   ))}
--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -5,6 +5,7 @@ const Task = (props) => {
        return (
         <div className="card">
             <p className="title">{props.title}</p>
+            <p>Priority: {props.priority}</p>
             <p>Due: {props.deadline}</p>
             <p className="describe">{props.description}</p>
         </div>
